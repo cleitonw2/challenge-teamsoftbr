7b57446bd2728ca50ca1a6d566a9a9d40c27706b
Cleiton <cleitonwoycik@outlook.com>
2022-04-30 17:30:51 -0300

    feat: ensure AddCustomer returns false if AddCustomerRepo returns false

--- src/domain/usecases/add-customer-usecase.ts
+++ src/domain/usecases/add-customer-usecase.ts
@@ -5,6 +5,5 @@ export const addCustomerUseCase =
   (chekCnpjRepo: CheckCnpjRepo, addCustomerRepo: AddCustomerRepo) =>
     async (data: Customer) => {
       const customerExists = await chekCnpjRepo.checkCnpj(data.cnpj)
-      if (customerExists) return false
-      await addCustomerRepo.add(data)
+      return customerExists ? false : addCustomerRepo.add(data)
     }
