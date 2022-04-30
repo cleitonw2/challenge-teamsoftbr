09bfd9f0e66c008cf22d653ca715791dde1f1200
Cleiton <cleitonwoycik@outlook.com>
2022-04-30 17:42:32 -0300

    refactor: add types in AddCustomerUseCase

--- src/domain/usecases/add-customer-usecase.ts
+++ src/domain/usecases/add-customer-usecase.ts
@@ -1,9 +1,14 @@
 import { AddCustomerRepo, CheckCnpjRepo } from '../contracts'
 import { Customer } from '../entities'
 
+type Params = { params: Customer }
+type Result = { result: boolean }
+
+export type AddCustomerUseCase = (params: Params) => Promise<Result>
+
 export const addCustomerUseCase =
   (chekCnpjRepo: CheckCnpjRepo, addCustomerRepo: AddCustomerRepo) =>
-    async (data: Customer) => {
-      const customerExists = await chekCnpjRepo.checkCnpj(data.cnpj)
-      return customerExists ? false : addCustomerRepo.add(data)
+    async (params: Customer) => {
+      const customerExists = await chekCnpjRepo.checkCnpj(params.cnpj)
+      return customerExists ? false : addCustomerRepo.add(params)
     }
