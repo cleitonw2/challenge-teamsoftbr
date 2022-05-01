9abc51941000d67362f93e751af962979b4d4bfa
Cleiton <cleitonwoycik@outlook.com>
2022-05-01 16:05:43 -0300

    feat: add customer repository

---
+++ src/infra/db/customer-repo.ts
@@ -0,0 +1,38 @@
+import { AddContentRepo, CheckContentRepo, LoadContentRepo, DeleteContentRepo, UpdateContentRepo } from '@/domain/contracts'
+import { Customer } from '@/domain/entities'
+import { MongoHelper } from './mongo-helper'
+
+export class CustomerRepo implements AddContentRepo, CheckContentRepo, LoadContentRepo,
+DeleteContentRepo, UpdateContentRepo {
+  async add (params: Customer): Promise<boolean> {
+    const customersCollection = await MongoHelper.getCollection('customers')
+    return !!await customersCollection.insertOne(params)
+  }
+
+  async check ({ cnpj }: { cnpj: string }): Promise<boolean> {
+    const customersCollection = await MongoHelper.getCollection('customers')
+    return !!await customersCollection.findOne({ cnpj })
+  }
+
+  async load (cnpj: string): Promise<Customer> {
+    const customersCollection = await MongoHelper.getCollection('customers')
+    const collection = await customersCollection.findOne({ customerCnpj: cnpj })
+    return collection ? MongoHelper.map(collection) : false
+  }
+
+  async update (params: Customer): Promise<void> {
+    const { cnpj, ...rest } = params
+    const customersCollection = await MongoHelper.getCollection('customers')
+    await customersCollection.updateOne(
+      { cnpj },
+      {
+        $set: { ...rest }
+      }
+    )
+  }
+
+  async delete (cnpj: string): Promise<void> {
+    const customersCollection = await MongoHelper.getCollection('customers')
+    await customersCollection.deleteOne({ cnpj })
+  }
+}
